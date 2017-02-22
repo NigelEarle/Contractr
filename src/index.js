63f5d5e505ef31cff0b6144ffa27361378ad2027
nwre87 <nwre87@gmail.com>
2017-02-21 17:52:52 -1000

    add google oauth - simple store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { facebook } from 'react-native-simple-auth';
+import { facebook, google } from 'react-native-simple-auth';
+import { Facebook, Google } from './config/config.json';
 import {
   AppRegistry,
   StyleSheet,
@@ -30,15 +31,21 @@ const styles = StyleSheet.create({
 class Contractr extends Component {
   constructor(props) {
     super(props);
-    this.handlePress = this.handlePress.bind(this);
+    this.handleFbLogin = this.handleFbLogin.bind(this);
+    this.handleGoogleLogin = this.handleGoogleLogin.bind(this);
+
+    this.state = {
+      data: '',
+    };
   }
 
-  handlePress() {
-    const config = {
-      appId: '1851993958375026',
-      callback: 'fb1851993958375026://authorize',
+  handleFbLogin() {
+    const FacebookConfig = {
+      appId: Facebook.appId,
+      callback: Facebook.callback,
     };
-    facebook(config)
+
+    facebook(FacebookConfig)
     .then((info) => {
       this.setState({
         data: info
@@ -49,15 +56,37 @@ class Contractr extends Component {
     });
   }
 
+  handleGoogleLogin() {
+    const GoogleConfig = {
+      appId: Google.appId,
+      callback: Google.callback,
+    };
+
+    google(GoogleConfig)
+    .then((result) => {
+      this.setState({
+        data: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
         <Button
-          onPress={this.handlePress}
+          onPress={this.handleFbLogin}
           title="Facebook"
+        />
+        <Button
+          onPress={this.handleGoogleLogin}
+          title="Google"
         />
         <Text style={styles.instructions}>
           Press Cmd to reload,{'\n'}
