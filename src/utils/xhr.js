db47665b7ce15e45c1a2d7c799b2dfd92ba8daba
nwre87 <nwre87@gmail.com>
2017-02-25 11:12:40 -1000

    add rough xhrUtil func - remove reqest, add request-promise

--- src/utils/xhr.js
+++ src/utils/xhr.js
@@ -1,1 +1,35 @@
-export default 'xhrUtil';
+import rp from 'request-promise';
+
+export default ({
+  endpoint = '/api', // adjust for env - config
+  path = '/',
+  method = 'GET',
+  body,
+  accessToken,
+}) => {
+  const options = {
+    method,
+    uri: `http://localhost:3000${endpoint}${path}`, // adjust for env - config
+    headers: {
+      'content-type': 'application/json;charset=UTF-8'
+    },
+  };
+
+  if (accessToken) {
+    options.access_token = accessToken;
+  }
+
+  if (body) {
+    options.json = true;
+    options.body = {};
+    // set dyanmic body key values
+  }
+
+  return rp(options)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
